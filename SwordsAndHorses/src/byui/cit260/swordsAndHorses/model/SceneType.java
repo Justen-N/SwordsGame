25d6474fd37d2e8f8001284a93e1b74a4c0b9003
galvanet <eduardo.dbf@gmail.com>
2017-03-15 23:38:02 -0600

    Add files via upload

---
+++ SwordsAndHorses/src/byui/cit260/swordsAndHorses/model/SceneType.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.swordsAndHorses.model;
+
+/**
+ *
+ * @author Eduardo y Sandra
+ */
+public enum SceneType {
+    
+    throneRoom,
+    starterZone,
+    misteryTower,
+    ogreIsland,
+    magiciansHut,
+    dragonCave,
+    
+}
